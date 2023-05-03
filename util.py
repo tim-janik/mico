9b689e0de5689335aa7770979f07e1e890010333
Tim Janik <timj@gnu.org>
2023-05-03 19:34:56 +0200

    util.py: add collect_files()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- util.py
+++ util.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
-import sys
+import sys, os
 
 # == Bunch ==
 class Bunch: # simplified object notation
@@ -8,3 +8,24 @@ class Bunch: # simplified object notation
     self.__dict__.update (kw)
   def __str__ (self):
     return str (vars (self))
+
+# == collect_files ==
+def collect_files (where, extension = None):
+  # match extension(s)
+  extensions = tuple (as_list (extension)) if extension else None
+  def matches (path):
+    if not extensions:
+      return True
+    return path.lower().endswith (extensions)
+  # recurse into path(s)
+  collected = []
+  for path in as_list (where):
+    if os.path.isdir (path):
+      for dirpath, dirnames, filenames in os.walk (path):
+        for filename in filenames:
+          if matches (filename):
+            collected.append (os.path.join (dirpath, filename))
+    elif matches (path) and os.path.exists (path):
+      collected.append (path)
+  return collected
+
