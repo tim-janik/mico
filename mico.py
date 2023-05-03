6b03e43dc91f78cd724d14b83778dac1aecacc4f
Tim Janik <timj@gnu.org>
2023-05-03 19:36:10 +0200

    mico.py: add --collect and --extension
    Signed-off-by: Tim Janik <timj@gnu.org>

--- mico.py
+++ mico.py
@@ -8,11 +8,13 @@ Usage: mico.py [--dump midifile]
 # == imports ==
 import sys, argparse, os, re
 import mido
-from util import Bunch
+from util import Bunch, as_list, collect_files
 
 # == CONFIG ==
 CONFIG = Bunch (
   dump = "",
+  extension = [],
+  collect = [],
   verbose = 0,
 )
 
@@ -21,6 +23,8 @@ def parse_options ():
   p = argparse.ArgumentParser (description = __doc__)
   a = p.add_argument
   a ('--dump', type = str, default = CONFIG.dump, help = "Dump MIDI file events")
+  a ('--collect', default = CONFIG.collect, action = 'append', help = "Collect files recursively")
+  a ('--extension', default = CONFIG.extension, action = 'append', help = "Only collect files matching extension")
   a ('-v', '--verbose', default = CONFIG.verbose, action = 'store_true', dest = 'verbose',
      help = "Increase output messages or debugging info")
   return p.parse_args()
@@ -31,6 +35,9 @@ def _main (argv):
   CONFIG = parse_options()
   if CONFIG.dump:
     print (mido.MidiFile (CONFIG.dump, clip = True))
+    sys.exit (0)
+  elif CONFIG.collect:
+    print ('\n'.join (collect_files (CONFIG.collect, CONFIG.extension)))
     sys.exit (0)
   print (__doc__)
   sys.exit (0)
